0089e875eaebf49554c97a5499bf96c5790a42a5
sohaibsalman <sohaibsalman10@gmail.com>
2022-10-22 20:43:06 +0500

    Add db module

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { HeartbeatController } from './heartbeat/heartbeat.controller';
+import { AuthModule } from './auth/auth.module';
+import { DbModule } from './db/db.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, DbModule, ConfigModule.forRoot({ isGlobal: true })],
   controllers: [HeartbeatController],
   providers: [],
 })
