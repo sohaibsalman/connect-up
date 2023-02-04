7b512243ffc2bebdf528db7144b7fbf0cd018337
sohaibsalman <sohaibsalman10@gmail.com>
2023-02-04 17:29:13 +0500

    CU-31: Create Custom Data Annotations for DTO validations

---
+++ api/ConnectUp.Models/DataAnnotations/PasswordAttribute.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace ConnectUp.Models.DataAnnotations
+{
+    internal class PasswordAttribute : ValidationAttribute
+    {
+        public PasswordAttribute() : base() { }
+
+        public override bool IsValid(object? value)
+        {
+            if (value is null) return false;
+
+            string password = (string)value;
+            password = password.Trim();
+            base.ErrorMessage = "Password must contain at least one ";
+
+            bool isValid = true;
+            if(!password.Any(char.IsUpper))
+            {
+                isValid = false;
+                base.ErrorMessage += "uppercase letter, ";
+            }
+            if(!password.Any(char.IsLower))
+            {
+                isValid = false;
+                base.ErrorMessage += "lowercase letter, ";
+            }
+            if (!password.Any(char.IsDigit))
+            {
+                isValid = false;
+                base.ErrorMessage += "digit, ";
+            }
+
+            base.ErrorMessage = isValid ? "" : base.ErrorMessage.Trim().Remove(base.ErrorMessage.LastIndexOf(","));
+            return isValid;
+        }
+    }
+}
